0b5f73102807c6a51e14665c9e448780589eba1c
Khilan Vitthani <thummar0211@gmail.com>
2025-08-04 15:22:21 +0530

    AppBar issue fix

--- lib/app/modules/setting_screen/views/setting_screen_view.dart
+++ lib/app/modules/setting_screen/views/setting_screen_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:managerapp/app/constants/color_constant.dart';
 
 import '../controllers/setting_screen_controller.dart';
 
@@ -8,7 +9,12 @@ class SettingScreenView extends GetView<SettingScreenController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Setting'), centerTitle: true),
+      backgroundColor: ColorConstants.bgColor,
+      appBar: AppBar(
+        title: const Text('Setting'),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
     );
   }
 }
