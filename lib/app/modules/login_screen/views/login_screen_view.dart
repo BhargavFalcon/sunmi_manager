179de67aee0e717516f0db4b4330bbfcbcf94044
Khilan Vitthani <thummar0211@gmail.com>
2025-08-05 12:30:38 +0530

    login screen done

---
+++ lib/app/modules/login_screen/views/login_screen_view.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:managerapp/app/constants/color_constant.dart';
+import 'package:managerapp/app/constants/sizeConstant.dart';
+
+import '../controllers/login_screen_controller.dart';
+
+class LoginScreenView extends GetView<LoginScreenController> {
+  const LoginScreenView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    MySize().init(context);
+    return Scaffold(
+      backgroundColor: ColorConstants.bgColor,
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(
+              12,
+            ).copyWith(top: MediaQuery.of(context).padding.top + 12),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: ColorConstants.getShadow2,
+            ),
+            child: Center(
+              child: Text(
+                "Login",
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MySize.screenWidth,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: ColorConstants.getShadow2,
+                    ),
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Login",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Container(
+                          height: MySize.getHeight(1),
+                          width: MySize.getWidth(40),
+                          color: ColorConstants.primaryColor,
+                        ),
+                        SizedBox(height: MySize.getHeight(2)),
+                        Text(
+                          "Welcome back! Please enter your details.",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: ColorConstants.grey600,
+                          ),
+                        ),
+                        SizedBox(height: MySize.getHeight(10)),
+                        Text(
+                          "Enter your email or username",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: MySize.getHeight(2)),
+                        CupertinoTextField(
+                          padding: const EdgeInsets.all(12),
+                          placeholder: "Email or Username",
+                          placeholderStyle: TextStyle(
+                            color: ColorConstants.grey600,
+                            fontSize: 14,
+                          ),
+                          style: TextStyle(color: Colors.black, fontSize: 14),
+                          decoration: BoxDecoration(
+                            color: ColorConstants.bgColor,
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(
+                              color: ColorConstants.grey600,
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: MySize.getHeight(2)),
+                        Text(
+                          "Enter your password",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: MySize.getHeight(2)),
+                        CupertinoTextField(
+                          padding: const EdgeInsets.all(12),
+                          placeholder: "Password",
+                          placeholderStyle: TextStyle(
+                            color: ColorConstants.grey600,
+                            fontSize: 14,
+                          ),
+                          style: TextStyle(color: Colors.black, fontSize: 14),
+                          obscureText: true,
+                          decoration: BoxDecoration(
+                            color: ColorConstants.bgColor,
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(
+                              color: ColorConstants.grey600,
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: MySize.getHeight(20)),
+                        Container(
+                          width: MySize.screenWidth,
+                          height: MySize.getHeight(35),
+                          decoration: BoxDecoration(
+                            color: ColorConstants.primaryColor,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Login",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
